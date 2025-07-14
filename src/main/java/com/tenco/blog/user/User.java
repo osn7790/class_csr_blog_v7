eed2644b135971ad1aa60ad2de49680e5dcaa274
osn7790 <oseungun311@gmail.com>
2025-07-15 08:28:28 +0900

    first commit

---
+++ src/main/java/com/tenco/blog/user/User.java
@@ -0,0 +1,55 @@
+package com.tenco.blog.user;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@Data
+@Table(name = "user_tb")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 사용자 이름 중복 방지를 위한 유니크 제약 설정
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+    private String email;
+    // now() -> x
+    // 엔티티가 영속화 될 때 자동으로 pc 현재시간을 설정해 준다
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    // 객체 생성시 가독성과 안정성 향상
+    @Builder
+    public User(Long id, String username, String password, String email, Timestamp createdAt) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
+
+    public void update(UserRequest.UpdateDTO updateDTO) {
+        updateDTO.validate();
+
+        // 영속 상태 엔티티의 필드값을 변경
+        this.password = updateDTO.getPassword();
+        this.email = updateDTO.getPassword();
+
+        // 변경 감시(Dirty checking)
+        // 1. 영속성 컨텍스트가 최초 상태를 스냅샷 보관
+        // 2. 필드값 변 시 현재 상태와 스냅샷 상태를 비교
+        // 3. 트랜잭션 커밋이 되면 변경된 필드만 UPDATE 쿼리 자동 생성
+        // 4. 반영 처리
+    }
+}
---
+++ src/main/java/com/tenco/blog/user/UserService.java
@@ -0,0 +1,83 @@
+package com.tenco.blog.user;
+
+import com.tenco.blog._core.errors.exception.Exception400;
+import com.tenco.blog._core.errors.exception.Exception404;
+import lombok.RequiredArgsConstructor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service // IoC
+@Transactional(readOnly = true) // 클래스 레벨에서의 읽기 전용 설정
+public class UserService {
+
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
+    private final UserJpaRepository userJpaRepository;
+
+
+    /**
+     * 회원가입 처리 - DTO 변환 책임
+     */
+    @Transactional // 메서드 레벨에서 쓰기 전용 트랜잭션 활성화
+    public UserResponse.JoinDTO join(UserRequest.JoinDTO joinDTO) {
+        //1. 사용자명 중복 체크
+        userJpaRepository.findByUsername(joinDTO.getUsername())
+                .ifPresent(user1 -> {
+                    throw new Exception400("이미 존재하는 사용자명입니다");
+                });
+        // 2. 회원 가입 처리
+        User savedUser = userJpaRepository.save(joinDTO.toEntity());
+        // 3. 응답 DTO로 변환 해서 반환 일을 시킴
+        return  new UserResponse.JoinDTO(savedUser);
+    }
+
+    /**
+     * 로그인 처리 - 응답 DTO 변환 책임
+     */
+    public UserResponse.LoginDTO login(UserRequest.LoginDTO loginDTO) {
+        // 회원 정보 조회
+        User selectedUser = userJpaRepository
+                .findByUsernameAndPassword(loginDTO.getUsername(), loginDTO.getPassword())
+                .orElseThrow(() -> {
+                    throw new Exception400("사용자명 또는 비밀번호가 틀렸어요");
+                });
+        // User 엔티티를 응답 DTO 변환해서 반환 처리
+        return new UserResponse.LoginDTO(selectedUser);
+    }
+
+    /**
+     *  사용자 정보 조회 - 응답 DTO 반환 책임
+     */
+    public UserResponse.DetailDTO findById(Long id) {
+        // 인증 처리 --> 세션 기반 (JWT 추후 변경)
+        // 권한 검사 일단 생략 ...
+        User selectedUser = userJpaRepository.findById(id).orElseThrow(() -> {
+            log.warn("사용자 조회 실패 - ID {}", id);
+            return new Exception404("사용자를 찾을 수 없습니다");
+        });
+        // 응답 DTO 변환 처리
+         return new UserResponse.DetailDTO(selectedUser);
+    }
+
+    /**
+     *  회원정보 수정 처리 - DTO 변환 책임
+     */
+    @Transactional
+    public UserResponse.UpdateDTO updateById(Long userId, UserRequest.UpdateDTO updateDTO) {
+        log.info("회원정보 수정 서비스 처리 시작 - ID : {}",  userId);
+
+        // 1. 권한 체크
+        User selectedUser = userJpaRepository.findById(userId)
+                .orElseThrow(() -> {
+                    throw new Exception404("사용자를 찾을 수 없습니다");
+                });
+        // 2. 더티 체킹을 통한 회원 정보 수정
+        selectedUser.update(updateDTO);
+
+        // 3. 응답 DTO 반환
+        return new UserResponse.UpdateDTO(selectedUser);
+    }
+}
