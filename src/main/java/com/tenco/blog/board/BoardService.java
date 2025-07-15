8802e608410cd9d16d9939cedf6df74b72b61408
osn7790 <oseungun311@gmail.com>
2025-07-15 15:17:29 +0900

    레스트컨트롤러 수정완료

--- src/main/java/com/tenco/blog/board/BoardService.java
+++ src/main/java/com/tenco/blog/board/BoardService.java
@@ -7,13 +7,17 @@ import com.tenco.blog.user.User;
 import com.tenco.blog.utils.Define;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -30,117 +34,49 @@ public class BoardService {
     private static final Logger log = LoggerFactory.getLogger(BoardService.class);
     private final BoardJpaRepository boardJpaRepository;
 
+
+    /**
+     * 게시글 목록 조회 - 페이징 처리
+     */
+    public List<BoardResponse.MainDTO> list(int page, int size) {
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by("id").descending());
+        Page<Board> boardPage = boardJpaRepository.findAllJoinUser(pageable);
+        List<BoardResponse.MainDTO> boardList = new ArrayList<>();
+        for (Board board : boardPage.getContent()) {
+            BoardResponse.MainDTO mainDTO = new BoardResponse.MainDTO(board);
+
+            boardList.add(mainDTO);
+        }
+
+        return boardList;
+    }
+
+    // 게시글 상세보기 - DTO  변환 책임(댓글 포함)
+    // Request ---> 컨트롤러 단 (토큰(JWT)정보 <- User)
+    public BoardResponse.DetailDTO detail(Long id, User sessionUser) {
+        // 1. 게시글 조회
+        Board board = boardJpaRepository.findByIdJoinUser(id).orElseThrow(
+                () -> new Exception404("게시글을 찾을 수 없습니다"));
+        // DTO 변환 책임 ( 댓글 정보 DTO 안에서 처리)
+        return new BoardResponse.DetailDTO(board, sessionUser);
+    }
+
     /**
      * 게시글 저장
      */
     // 메서드 레벨에서의 트랜잭선 선언
     @Transactional // 데이 수정이 필요하는 읽지 전용 설정을 해제하고 쓰기 전용로 변환
-    public Board save(BoardRequest.SaveDTO saveDTO, User sessionUser) {
-        // 1. 로그 기록 - 게시글 저장 요청 정보
-        // 2. DTO를 Entity로 변환(작성자 정보 포함)
-        // 3. 데이터베이스에 게시글 저장
-        // 4. 저장 완료 로그 기록
-        // 5. 저장된 Board 를 Controller 로 반환
-        log.info("게시글 저장 서비스 처리 시작 - 제목 {} , 작성자 {}",
-                saveDTO.getTitle(), sessionUser.getUsername());
+    public BoardResponse.SaveDTO save(BoardRequest.SaveDTO saveDTO, User sessionUser) {
+
+        // 영속성 컨텍스 [ 1차 캐시, 쓰기 지연 개념 ]
+        // 영속상태? 비영속상태일까? -> 비영속상태
         Board board = saveDTO.toEntity(sessionUser);
-        boardJpaRepository.save(board);
-        log.info("게시글 저장 완료 - ID {} , 제목 {}",
-                board.getId(), board.getTitle());
-        return  board;
-    }
+        // JPA 저장처리
+        Board savedBoard = boardJpaRepository.save(board); // 영속성 컨텍스트에서 관리가 됨
 
-    /**
-     * 게시글 목록 조회 - 페이징 처리
-     */
-    public Page<Board> findAll(Pageable pageable) {
-        // 1. 로그 기록
-        // 2. 데이베이스 게시글 조회
-        // 3. 로그 기록
-        // 4. 조회된 게시글 목록 반환
-        log.info("게시글 조회 서비스 처리 시작");
-        Page<Board> boardPage = boardJpaRepository.findAllJoinUser(pageable);
-        log.info("게시글 목록 조회 완료 - 토탈 게시글 수 {} 개, 토탈 페이지 {}",
-                boardPage.getTotalElements(), boardPage.getTotalPages());
-        return boardPage;
-    }
-
-    //
-    public Board findByIdWithReplies(Long id, User sessionUser) {
-        // 1. 게시글 조회
-        Board board =  boardJpaRepository.findByIdJoinUser(id).orElseThrow(
-                 () -> new Exception404("게시글을 찾을 수 없습니다"));
-        // 2. 게시글 작성자 정보 포함해 주어야 함
-        // 3. 게시글 소유권 설정(수정/ 삭제버튼 표 시용)
-        if(sessionUser != null) {
-            boolean isBoardOwner = board.isOwner(sessionUser.getId());
-            // 로직 : 메서드를 통해서 게시글 소유자를 확인하고
-            // 그 결과 값을 Board 객체에 담아 둔다
-            board.setBoardOwner(isBoardOwner);
-        }
-
-        // 댓글 정보 <-- 양방향 설정 Board <--- 댓글 가지옴
-        List<Reply> replies = board.getReplies();
-
-        // 댓글 소유권 설정 (삭제 버튼 표시용)
-        if(sessionUser != null) {
-//            for(int i = 0; i < replies.size(); i++) { }
-            replies.forEach(reply -> {
-                boolean isReplyOwner = reply.isOwner(sessionUser.getId());
-                reply.setReplyOwner(isReplyOwner);
-            });
-        }
-        return board;
-    }
-
-
-    /**
-     * 게시글 상세 조회
-     */
-    public Board findById(Long id) {
-        // 1. 로그 기록
-        // 2. 데이터 베스에서 해당 board id 로 조회 -  WHERE
-        // 3. 게시글이 없다면 404 에러 처리
-        // 4. 조회 성공시 로그 기록
-        // 5. 조회된 게시글 반환
-        log.info("게시글 상세 조회 서비스 시작 - ID {}", id);
-        Board board = boardJpaRepository.findByIdJoinUser(id).orElseThrow(() -> {
-            log.warn("게시글 조회 실패 - ID {}", id);
-            return new Exception404("게시글을 찾을 수 없습니다");
-        });
-        log.info("게시글 상세 조회 완료 - 제목 {}", board.getTitle());
-        return board;
-    }
-
-    /**
-     *  게시글 수정(권한 체크 포함)
-     */
-    @Transactional
-    public Board updateById(Long id, BoardRequest.UpdateDTO updateDTO,
-                            User sessionUser) {
-        // 1. 로그 기록
-        // 2. 수정하려는 게시글 조회
-        // 3. 권한 체크
-        // 4. 권한이 없다면 403 예외 발생
-        // 5. Board 엔티티에 상태값 변경 (더티 체팅)
-        // 6. 로그 기록 - 수정 완료
-        // 7. 수정된 게시글 반환
-        log.info("게시글 수정 서비스 시작 - 게시글 ID {}", id);
-        Board board = boardJpaRepository.findById(id).orElseThrow(() -> {
-            log.warn("게시글 조회 실패 - ID {}", id);
-            return new Exception404("해당 게시글이 존재하지 않습니다");
-        });
-
-        if(!board.isOwner(sessionUser.getId())) {
-            throw new Exception403("본인이 작성한 게시글만 수정 가능");
-        }
-
-        board.setTitle(updateDTO.getTitle()); // 필드값 상태 변경
-        board.setContent(updateDTO.getContent()); // 필드값 상태 변경
-        // TODO - board 엔티티에 update() 만들어 주기
-        // 더티 체킹
-        log.info("게시글 수정 완료 - 게시글 ID {}, 게시글 제목 {}", id, board.getTitle());
-        return board;
+        // 응답 DTO 변환 처리
+        return new BoardResponse.SaveDTO(board);
     }
 
     /**
@@ -148,30 +84,40 @@ public class BoardService {
      */
     @Transactional
     public void deleteById(Long id, User sessionUser) {
-        // 1. 로그 기록
-        // 2. 삭제 하려는 게시글 조회
-        // 3. 권한 체크
-        // 4. 권한이 없으면 403 예외 처리
-        // 5. 데이터 베이스 삭제 처리
-        // 6. 삭제 완료 로그 기록
-        log.info("게시글 삭제 서비스 시작 - ID {}", id);
+
         Board board = boardJpaRepository.findById(id).orElseThrow(() -> {
             return new Exception404("삭제하려는 게시글이 없습니다");
         });
-        if(!board.isOwner(sessionUser.getId())) {
+        if (!board.isOwner(sessionUser.getId())) {
             throw new Exception403("본인이 작성한 게시글만 삭제할 수 있습니다");
         }
+        // 게시글 삭제 ( 백엔드에서 repose.ok를 보내면 프론트에서 삭제되었다는 메시지만보내면됨)
         boardJpaRepository.deleteById(id);
     }
 
+
     /**
-     *  게시글 소유자 확인 (수정 화면 요청 확인용)
+     * 게시글 수정(권한 체크 포함)
      */
-    public void checkBoardOwner(Long boardId, Long userId) {
-        Board board = findById(boardId);
-        if(!board.isOwner(userId)) {
-            throw new Exception403("본인 게시글만 수정할 수 있습니다.");
+    @Transactional
+    public BoardResponse.UdateDTO udate(Long id, BoardRequest.UpdateDTO updateDTO,
+                                        User sessionUser) {
+
+        Board board = boardJpaRepository.findById(id).orElseThrow(() -> {
+            log.warn("게시글 조회 실패 - ID {}", id);
+            return new Exception404("해당 게시글이 존재하지 않습니다");
+        });
+
+        if (!board.isOwner(sessionUser.getId())) {
+            throw new Exception403("본인이 작성한 게시글만 수정 가능");
         }
+
+        // 더티 체킹 수정 처리
+        board.update(updateDTO);
+        // 더티 체킹
+        log.info("게시글 수정 완료 - 게시글 ID {}, 게시글 제목 {}", id, board.getTitle());
+        return new BoardResponse.UdateDTO(board);
     }
+
 
 }
